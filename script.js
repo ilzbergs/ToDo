9fb4c09935cb41c2820ae472a91d9839e4b168c9
ilzbergs <85389433+ilzbergs@users.noreply.github.com>
2022-01-29 15:20:41 +0200

    done css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 var completedToDo = document.getElementById('completedToDos');
 var btn = document.querySelector('button');
-var inputOutlineColor=document.querySelector('input');
+var inputOutlineColor = document.querySelector('input');
 const todos = JSON.parse(localStorage.getItem('todos'));
 const color = JSON.parse(localStorage.getItem('x'));
 
@@ -35,37 +35,26 @@ function addTodo(todo) {
         todoEl.innerText = todoText;
 
         //add to completed 
-            todoEl.addEventListener('click', () => {
-                // completedToDo.appendChild(todoEl);
-                todoEl.classList.add('completed');
-                updateLS();
-            });
+        todoEl.addEventListener('click', () => {
+            // completedToDo.appendChild(todoEl);
+            todoEl.classList.add('completed');      
+        });
 
         //delete to dos
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
-            todoEl.remove();
-            updateLS();
+            todoEl.classList.add('deleting');
+            window.setTimeout(function(e) {
+                todoEl.remove();
+                 updateLS();
+              }, 500);
+    
+           
         });
         todosUL.appendChild(todoEl);
         input.value = '';
         updateLS();
     }
-}
-//color change heading
-var h1 = document.getElementById('h1');
-h1.addEventListener('click', () => {
-    h1.style.color = ColorCode();
-    btn.style.backgroundColor= h1.style.color;
-    inputOutlineColor.style.outlineColor = h1.style.color;
-});
-function ColorCode() {
-    var makingColorCode = '0123456789ABCDEF';
-    var finalCode = '#';
-    for (var counter = 0; counter < 6; counter++) {
-        finalCode = finalCode + makingColorCode[Math.floor(Math.random() * 16)];
-    }
-    return finalCode;
 }
 
 
@@ -86,7 +75,61 @@ function updateLS() {
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 
+const numBalls = 50;
+const balls = [];
 
+for (let i = 0; i < numBalls; i++) {
+    let ball = document.createElement("div");
+    ball.classList.add("ball");
+    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+    ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+    ball.style.transform = `scale(${Math.random()})`;
+    ball.style.width = `${Math.random()}em`;
+    ball.style.height = ball.style.width;
 
+    balls.push(ball);
+    document.body.append(ball);
+}
+;
+balls.forEach((el, i, ra) => {
+    let to = {
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
 
+        y:Math.random()*5
+    };
+
+    let anim = el.animate(
+        [
+            { transform: "translate(0, 0)" },
+            { transform: `translate(${to.x}rem, ${to.y}rem`}
+        ],
+        {
+            duration: (Math.random() + 1) * 2000,
+            direction: "alternate",
+            fill: "both",
+            iterations: Infinity,
+            easing: "ease-in-out"
+        }
+    );
+});
+
+//color change heading, button and input focus
+var h1 = document.getElementById('h1');
+h1.addEventListener('click', () => {
+    h1.style.color = ColorCode();
+    btn.style.backgroundColor = h1.style.color;
+    inputOutlineColor.style.outlineColor = h1.style.color;
+   
+});
+function ColorCode() {
+    var makingColorCode = '0123456789ABCDEF';
+    var finalCode = '#';
+    for (var counter = 0; counter < 6; counter++) {
+        finalCode = finalCode + makingColorCode[Math.floor(Math.random() * 16)];
+    }
+    return finalCode;
+}
